1bd8462fd8bae8b4484044ab433914846f19ef1f
Victor Mutai <victormutaijambo@gmail.com>
2026-09-05 13:35:38 +0300

    Initial Commit

---
+++ mtihani-macos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mtihani-macos
+//
+//  Created by Victor Mutai on 05/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mtihani-macos/mtihani_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  mtihani_macosApp.swift
+//  mtihani-macos
+//
+//  Created by Victor Mutai on 05/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct mtihani_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
